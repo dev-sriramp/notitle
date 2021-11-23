cc74e1e42ab2984321b101bf8cee98ebbbac1bad
SRIRAM <uniqueredhat@gmail.com>
2021-11-23 22:46:41 +0530

    v6 react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import {
+  AuthProvider
+} from "./Components/Auth";
+import Home from "./Components/Home";
+
+const App = () => {
+  return ( <AuthProvider >
+    <Router >
+    <Routes >
+    <Route exact path = "/" element={<Navigate to="/Home" />} />
+     <Route exact path = "/Home" element={<Home />} />
+    </Routes >
+    </Router>
+    </AuthProvider>
   );
 }
 
