4bdd2769c7ce2f54dcf74e271969d3c1f35ab071
SRIRAM <uniqueredhat@gmail.com>
2021-11-23 21:20:34 +0530

    added fb

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuZ1ctwIHpp353zCJHyQ3m_5_ZtVqg0cQ",
+  authDomain: "notitle-b8e49.firebaseapp.com",
+  projectId: "notitle-b8e49",
+  storageBucket: "notitle-b8e49.appspot.com",
+  messagingSenderId: "851343607280",
+  appId: "1:851343607280:web:c2b2db79ebd1efac290935"
+};
+
+// Initialize Firebase
+const fireBase = initializeApp(firebaseConfig);
