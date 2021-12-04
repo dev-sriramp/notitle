e4d5052575d4d7c617c0964f5cdcbcd0e06da94e
SRIRAM <uniqueredhat@gmail.com>
2021-12-04 15:07:49 +0530

    some random

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,95 +1,89 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Taskinput from "./Taskinput";
 import { db } from "../../config";
-import { doc, setDoc } from "firebase/firestore";
+import { getDocs, collection } from "firebase/firestore";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 toast.configure()
 
 const Task = (props) => {
-  const [formValues, setFormValues] = useState([{ name: ""}])
-  let addFormFields = () => {
 
-        setFormValues([...formValues, { name: "" }])
-      }
-    let removeFormFields = (i) => {
-             let newFormValues = [...formValues];
-             newFormValues.splice(i, 1);
-             setFormValues(newFormValues)
-         }
+  const [formValues, setFormValues] = useState([{ date: "", workStation: "", model: "", count: "", timeTaken: "" ,modelProp:[]}])
 
-  const appendChildData = async (e) => {
-    e.preventDefault();
-    const props = e.target.elements;
-    console.log(props)
-    var len = props.length-2;
-    for(let i=0;i<len-1;i=i+6){
-     let today = new Date();
-     let counter = today.getTime() +""+ today.getDate() +""+ (today.getMonth()+1) +""+ today.getFullYear();
-     try{
-     await setDoc(doc(db, props[i].value, "#" + counter), {
-       date: props[i].value,
-       workstation: props[i+1].value,
-       substation: props[i+2].value,
-       count: props[i+3].value,
-       time: props[i+4].value,
-       id:"#" + counter,
-     });
-     toast.success('Task Assigned successfully');
+  let handleChange = async (i, e) => {
+    const res = [];
+
+    let newFormValues = [...formValues];
+    newFormValues[i][e.target.name] = e.target.value;
+
+    if (e.target.name === "workStation") {
+      var s = e.target.value;
+      const querySnapshot = await getDocs(collection(db, s));
+      querySnapshot.forEach((doc) => {
+        let s = doc.data();
+        res.push(s.model)
+
+      });
+      newFormValues[i]["modelProp"] = res;
+    }
+    setFormValues(newFormValues);
+    console.log(formValues)
   }
-     catch{
-       toast.error('Error Occurred');
-     }
-       await setDoc(doc(db, "total", props[i].value), {
-         date:props[i].value,
-       });}
 
-     }
-    return(
-        <div>
-          <h2 className="pt-1 ps-1"> Assign Task</h2>
-          <p className="ps-1 text-muted">You can add multiple fields and publish them in  single click.</p>
-        <div>
-            <div>
+  let addFormFields = () => {
+    setFormValues([...formValues, { date: "", workStation: "", model: "", count: "", timeTaken: "" ,modelProp:[]}])
+  }
+
+  let removeFormFields = (i) => {
+    let newFormValues = [...formValues];
+    newFormValues.splice(i, 1);
+    setFormValues(newFormValues)
+  }
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(JSON.stringify(formValues));
+  }
+
+  return (
+    <div>
+      <h2 className="pt-1 ps-1"> Assign Task</h2>
+
+      <p className="ps-1 text-muted">You can add multiple fields and publish them in  single click.</p>
+      <div>
         <div className="card border ">
-        <div className="card-body">
-        <div className="table-responsive ">
-        <form onSubmit={appendChildData} >
-        <table className="table table-bordered ">
-            <thead className="table table-dark">
-                <tr>
-                <th scope="col">No</th>
-                <th scope="col">Date</th>
-                <th scope="col">Workstartion</th>
-                <th scope="col">Model</th>
-                <th scope="col">Planned count</th>
-                <th scope="col">Planned time</th>
-                <th scope="col"> </th>
-                </tr>
-            </thead>
-              <tbody>
-        {formValues.map(( element,index) => (
-        <Taskinput value={props.info.sort()} keys={index} DeleteButton={removeFormFields} ></Taskinput>
-        ))}
-</tbody>
-        </table>
-        <center>
-            <button onClick={() => addFormFields()}type="button" className="btn btn-outline-secondary">
-            + Add field</button>
-        </center>
-        <center>
-        <div className=" mt-3" >
-            <button className="btn btn-primary"  style={{width:"22rem"}} type="submit">Publish</button>
+          <div className="card-body">
+            <div className="table-responsive ">
+              <form onSubmit={handleSubmit}>
+                <table className="table table-bordered ">
+                  <thead className="table table-dark">
+                    <tr>
+                      <th scope="col">No</th>
+                      <th scope="col">Date</th>
+                      <th scope="col">Workstartion</th>
+                      <th scope="col">Model</th>
+                      <th scope="col">Planned count</th>
+                      <th scope="col">Planned time</th>
+                      <th scope="col"> </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                     {formValues.map((element, index) => (
+                      <Taskinput key={index} index={index} element={element} handleChange={handleChange} info={props.info} removeFormFields={removeFormFields} />
+                  ))}
+                  </tbody>
+                </table>
+              <div className="button-section">
+                <center>
+                  <button className="btn btn-outline-secondary" type="button" onClick={() => addFormFields()}>+ Add field</button>
+                  <div className=" mt-3" ><button className="btn btn-primary" style={{ width: "22rem" }} type="submit">Publish</button>
+                  </div></center></div></form>
+            </div >
           </div>
-        </center>
-        </form>
-        </div>
-        </div>
-        </div>
-            </div>
-         </div>
-        </div>
-    )
+        </div >
+      </div>
+    </div>
+  )
 }
 export default Task;
--- src/Components/Task/Taskinput.js
+++ src/Components/Task/Taskinput.js
@@ -1,67 +1,43 @@
-import React,{useState} from "react";
-import { db } from "../../config";
-import { getDocs,collection } from "firebase/firestore";
+import React from "react";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {ReactComponent as Deletesvg} from '../../assets/delete.svg';
 toast.configure();
 
-
 const Taskinput = (props) =>{
-  const [date, setDate] = useState([]);
-  const [model, setModel] = useState([]);
-  const [productionUnit, setProductionUnit] = useState();
-  const [subUnit, setSubUnit] = useState();
-  const [count, setCount] = useState();
-  const [time, setTime] = useState();
 
-  const handleChange = async (e) => {
-    const res = [];
-    var s = e.target.value;
-    setProductionUnit(s);
-    const querySnapshot = await getDocs(collection(db, s));
-querySnapshot.forEach((doc) => {
-  let s = doc.data();
-  res.push(s.model)
-});
-setModel(res)
-  }
 
     return(
-                    <tr key={props.keys}>
-                    <th scope="row">{props.keys+1}</th>
-                    <td>
-                    <input type="date" required name="date" value={date} onChange={(e) => { setDate(e.value) }} className="form-control" id="exampleFormControlInput1" placeholder="count"></input>
-                    </td>
-                    <td>
-                    <select required className="form-select" name="workStation" value={productionUnit} onChange={(e)=>{handleChange(e)}} aria-label="Default select example">
-                            <option disabled selected>Select workstartion</option>
-                            {props.value.map((data) => (<option key={data} value={data}>{data}</option>))}
-                        </select>
-                    </td>
-                    <td>
-                    <select required className="form-select"  name="model" value={subUnit} onChange={(e) => { setSubUnit(e.value) }} aria-label="Default select example">
-                            <option disabled >Select Model</option>
-                            {model.map((data) => (<option key={data} value={data}>{data}</option>))}
-                        </select>
-                    </td>
-                    <td>
-                    <input required type="number" name="count" className="form-control" id="exampleFormControlInput1" value={count} onChange={(e) => { setCount(e.value) }} placeholder="count"></input>
-                    </td>
-                    <td>
-                    <input required type="time" name="time" value={time} onChange={(e) => { setTime(e.value) }} className="form-control" id="exampleFormControlInput1"></input>
-                    </td>
-                    <td>
-                    {
-                props.keys?
-                <button type="button" className="btn btn-white" onClick={(e)=>{props.DeleteButton(props.keys)}}>
-                <Deletesvg></Deletesvg>
-                </button> : <button type="button" disabled className="btn btn-White" onClick={(e)=>{props.DeleteButton(props.keys)}}>
-                <Deletesvg></Deletesvg>
-                </button>
-              }
-                    </td>
-                    </tr>
+      <tr key={props.index}>
+        <th scope="row">{props.index + 1}</th>
+        <td>
+          <input required type="date" name="date" value={props.element.date || ""} onChange={e => props.handleChange(props.index, e)} className="form-control" />
+        </td>
+        <td>
+          <select required className="form-select" name="workStation" value={props.element.workStation || ""} onChange={e => props.handleChange(props.index, e)} aria-label="Default select example" >
+            <option defaultValue="" >Select workstartion</option>
+            {props.info.sort().map((data) => (<option key={data} value={data}>{data}</option>))}
+          </select>
+        </td>
+        <td>
+          <select required className="form-select" name="model" value={props.element.model || ""} aria-label="Default select example" onChange={e => props.handleChange(props.index, e)} >
+            <option disabled defaultValue="">Select Model</option>
+            {props.element.modelProp.map((data) => (<option key={data} value={data}>{data}</option>))}
+          </select>
+        </td>
+        <td>
+          <input required className="form-control" type="number" name="count" value={props.element.count || ""} onChange={e => props.handleChange(props.index, e)} />
+        </td>
+        <td>
+          <input required className="form-control" type="time" name="timeTaken" value={props.element.timeTaken || ""} onChange={e => props.handleChange(props.index, e)} />
+        </td>
+        <td>   {
+          props.index ?
+            <button type="button" className="btn btn-white" onClick={() => props.removeFormFields(props.index)}>  <Deletesvg /></button>
+            : <button type="button" className="btn btn-white" disabled> <Deletesvg /></button>
+        }
+        </td>
+      </tr>
     )
 }
 export default Taskinput;
