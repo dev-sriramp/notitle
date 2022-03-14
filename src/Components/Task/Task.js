300c2f82a229887dd9623b5ba4cf0a8f499a3f25
SANJITH M <dev.sanjithm@gmail.com>
2022-03-14 02:35:05 -0700

    Update Task.js

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -5,12 +5,11 @@ import { setDoc, doc, getDocs, collection } from "firebase/firestore";
 import { toast } from 'react-toastify';
 import { DATE, WORK_STATION, WORK_STATION_MODEL, TOTAL_COUNT, TOTAL_TIME } from "../../constants/constants";
 import 'react-toastify/dist/ReactToastify.css';
-
+import * as xlsx from "xlsx";
 toast.configure()
 
 const Task = (props) => {
-
-
+  const [excelData,setexcelData] = useState();
   const [formValues, setFormValues] = useState([{ date: "", workStation: "", model: "", count: "", timeTaken: "", modelProp: [] }])
 
   let handleChange = async (i, e) => {
@@ -72,66 +71,41 @@ const Task = (props) => {
     }
     setFormValues([{ date: "", workStation: "", model: "", count: "", timeTaken: "", modelProp: [] }])
   }
+  //used to convert excel data to json
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = xlsx.utils.sheet_to_json(worksheet);
+        SortJson(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  }
+
+  function SortJson(json) {
+    const dataThreads = json.map((data) => {
+      return {
+        date: data.date,
+        workstation: data.workstation,
+        substation: data.substation,
+        count: data.count,
+        time: data.time,
+        model: "small"
+      };
+    })
+    setexcelData(dataThreads);
+  }
 
   return (
     <div>
       <h2 className="pt-1 ps-1"> Assign Task</h2>
       <p className="ps-1 text-muted">You can add multiple fields and publish them in  single click.</p>
-      <div class="container">
-        <div class="row align-items-start">
-          <div class="col">
-          </div>
-          <div class="col">
-          </div>
-          <div class="col">
-            <div class="position-relative">
-              <div class="position-absolute top-50 start-100 translate-middle">
-                <button type="button" class="btn btn-primary mb-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  Import
-                </button>
-              </div>
-            </div>
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Preview</h5>
-                    <button type="button" class="btn btn-success" >Choose file</button>
-                  </div>
-                  <div class="modal-body">
-                    <table class="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">Date</th>
-                          <th scope="col">Workstation</th>
-                          <th scope="col">Modal</th>
-                          <th scope="col">count</th>
-                          <th scope="col">time</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th scope="row">1</th>
-                          <td>10-12-2021</td>
-                          <td>Workstation 1</td>
-                          <td>small</td>
-                          <td>10</td>
-                          <td>10:55</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancle</button>
-                    <button type="button" class="btn btn-primary">Publish</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div>
         <div className="card border ">
           <div className="card-body">
