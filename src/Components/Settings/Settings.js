74dd723cf8b307712896110de10d3033ed91dcde
SRIRAM <60465112+dev-udhayakumar@users.noreply.github.com>
2022-03-24 11:25:25 +0530

    ui updated

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -86,120 +86,156 @@ setInfo(res);
 
     return(
         <div>
-             <Navbar home={"btn btn-black text-white me-3"} report={"btn btn-black text-white me-3"} settings={"btn btn-warning me-3"} ></Navbar>
+            <Navbar home={"btn btn-black text-white me-3"} report={"btn btn-black text-white me-3"} settings={"btn btn-warning me-3"} ></Navbar>
             <h2 className="ps-4 pt-1"> Settings</h2>
-            <div className="continer m-4" style={{ borderRadius: "12px", overflow: "scroll", height: "600px", boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)" }}>
-            <div class="row p-3 mt-5">
-                <div class="col-md-4">
-                <div className="card mb-3  ">
-                <div className="card-body">
-                <h6 className="mb-3">
-                 {WORK_STATION} Settings</h6>
-                    <div className="d-grid gap-2 mb-3">
-                    <button className="btn btn-outline-warning" type="button"  data-bs-toggle="modal" data-bs-target="#Workstationwindow">Add {WORK_STATION}</button>
-                        <button className="btn btn-dark" type="button"  data-bs-toggle="modal" data-bs-target="#" >Edit {WORK_STATION}</button>
-                    </div>
-                    <div className="modal fade" id="Workstationwindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                        <div className="modal-content">
+            <div class=" mt-5 container-sm">
+                <div class="card mb-3">
+                    <ul class="list-group list-group-flush">
+                        <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Add Workstation</div>
+                                Create a Workstartion for assigning  task
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#Workstationwindow">Add Workstartion</button>
+                        </li>
+                        {/* <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Delete Workstartion</div>
+                                Once you delete a Workstartion, there is no going back. Please be certain.
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger">Delete Workstartion</button>
+                        </li> */}
+
+                    </ul>
+                </div>
+                <div class="card  mb-3">
+                    <ul class="list-group list-group-flush">
+
+                        <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Add Model</div>
+                                Create a Model for assigning  task
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#Modelwindow">Add Model</button>
+                        </li>
+                        {/* <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Delete Model</div>
+                                Once you delete a model, there is no going back. Please be certain.
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger">Delete Model</button>
+                        </li> */}
+                    </ul>
+                </div>
+                <div class="card  mb-3">
+                    <ul class="list-group list-group-flush">
+
+                        <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Delete Task</div>
+                                Once you delete a Task, there is no going back. Please be certain.
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger" disabled>Delete Task</button>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="card  mb-3">
+                    <ul class="list-group list-group-flush">
+
+                        <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Change Password</div>
+                                This will change Password for a current User
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger" disabled >Change Password</button>
+                        </li>
+                        <li class=" list-group-item d-flex justify-content-between align-items-start">
+                            <div class="m-2  me-auto">
+                                <div class="fw-bold">Logout</div>
+                                Logout from the Applicaton
+                            </div>
+                            <button type="button" class="m-2 btn btn-outline-danger"  data-bs-toggle="modal" data-bs-target="#staticBackdrop">Logout</button>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className="modal fade" id="Workstationwindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="WorkstationwindowLable">Add {WORK_STATION}</h5>
                         </div>
                         <div className="modal-body">
-                        <div className="mb-3">
-                            <label for="exampleFormControlInput1" className="form-label">Name</label>
-                            <input required type="Name" value={addWorkStation} onChange={(e)=>{setAddWorkStation(e.target.value)}} className="form-control"placeholder="Workstation Name"></input>
+                            <div className="mb-3">
+                                <label for="exampleFormControlInput1" className="form-label">Name</label>
+                                <input required type="Name" value={addWorkStation} onChange={(e) => { setAddWorkStation(e.target.value) }} className="form-control" placeholder="Workstation Name"></input>
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-outline-warning" data-bs-dismiss="modal">Cancel</button>
                             <button type="button" className="btn btn-warning" onClick={appendChildData} data-bs-dismiss="modal">Add</button>
                         </div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
-                </div>
-                <div class="col-md-4">
-                <div className="card mb-3">
-                <div className="card-body">
-                <h6 className="mb-3">  {WORK_STATION_MODEL} Settings</h6>
-                    <div className="d-grid gap-2 mb-3">
-                    <button className="btn btn-outline-warning" type="button" data-bs-toggle="modal" data-bs-target="#Modelwindow">Add {WORK_STATION_MODEL} </button>
-                        <button className="btn btn-dark" type="button" data-bs-toggle="modal" data-bs-target="#" >Edit {WORK_STATION_MODEL}l </button>
-                    </div>
-                    <div className="modal fade" id="Modelwindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                        <div className="modal-content">
+
+            <div className="modal fade" id="Modelwindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="ModelwindowLable">Add {WORK_STATION_MODEL}</h5>
                         </div>
                         <div className="modal-body">
-                        <div className="mb-3">
-                        <label  className="form-label">Select {WORK_STATION}</label>
-                            <select className="form-select" value={workStation} onChange={(e)=>{setWorkStation(e.target.value)}} aria-label="Default select example">
-                                <option selected>Select workstartion</option>
-                                {info.sort().map((data) => (<option key={data} value={data}>{data}</option>))}
-                            </select>
-                        </div>
+                            <div className="mb-3">
+                                <label className="form-label">Select {WORK_STATION}</label>
+                                <select className="form-select" value={workStation} onChange={(e) => { setWorkStation(e.target.value) }} aria-label="Default select example">
+                                    <option selected>Select workstartion</option>
+                                    {info.sort().map((data) => (<option key={data} value={data}>{data}</option>))}
+                                </select>
+                            </div>
 
                             <div className="mb-3">
                                 <label className="form-label">{WORK_STATION_MODEL} Name</label>
-                                <input type="Name" value={model} onChange={(e)=>{setModel(e.target.value)}} className="form-control"placeholder="Name"></input>
+                                <input type="Name" value={model} onChange={(e) => { setModel(e.target.value) }} className="form-control" placeholder="Name"></input>
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-outline-warning" data-bs-dismiss="modal">Cancel</button>
                             <button type="button" className="btn btn-warning" onClick={appendModel} data-bs-dismiss="modal">Add</button>
                         </div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
-                </div>
-                <div class="col-md-4">
-                <div className="card mb-3">
-                <div className="card-body">
-                    <h6 className="mb-3"><Accountsvg></Accountsvg> Account Settings</h6>
-                <div className="d-grid mb-3 gap-2">
-                <button type="button"   className="btn btn-outline-warning ">
-                      Change Password 
-                        </button>
-                <button type="button"   className="btn btn-dark " data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                      Logout
-                        </button>
-                        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="staticBackdropLabel">Confirm Logout</h5>
-                            </div>
-                            <div className="modal-body">
-                                <div className="row">
+
+
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="staticBackdropLabel">Confirm Logout</h5>
+                        </div>
+                        <div className="modal-body">
+                            <div className="row">
                                 <div className="col">
-                            <div className="d-grid gap-2">
-                            <button className="btn btn-outline-warning" onClick={logOut} data-bs-dismiss="modal" type="button">Yes</button>
-                            </div>
-                            </div>
-                            <div className="col">
-                            <div className="d-grid gap-2">
-                            <button className="btn btn-warning" data-bs-dismiss="modal" type="button">No</button>
-                            </div>
-                            </div>
+                                    <div className="d-grid gap-2">
+                                        <button className="btn btn-outline-warning" onClick={logOut} data-bs-dismiss="modal" type="button">Yes</button>
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="d-grid gap-2">
+                                        <button className="btn btn-warning" data-bs-dismiss="modal" type="button">No</button>
+                                    </div>
                                 </div>
                             </div>
-                            </div>
                         </div>
-                        </div>
+                    </div>
                 </div>
+            </div>
 
-                </div>
-            </div>
-                </div>
-            </div>
-            </div>
 
         </div>
     )
